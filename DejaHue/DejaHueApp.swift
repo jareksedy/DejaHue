5dd59ca5721c782f386d097607f9af765aea3fec
Yaroslav Sedyshev <jareksedy@icloud.com>
2024-09-02 12:50:33 +0500

    Initial Commit

---
+++ DejaHue/DejaHueApp.swift
@@ -0,0 +1,17 @@
+//
+//  DejaHueApp.swift
+//  DejaHue
+//
+//  Created by Ярослав Седышев on 02.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct DejaHueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
